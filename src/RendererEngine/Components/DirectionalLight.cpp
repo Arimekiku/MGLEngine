9067c52a8e8a9fd745943bb33e1063810b78661a
Maxim <petsol.reserve228@gmail.com>
2024-10-11 00:52:18 +0300

    Fix clang import error

--- src/RendererEngine/Components/DirectionalLight.cpp
+++ src/RendererEngine/Components/DirectionalLight.cpp
@@ -8,7 +8,7 @@ namespace RenderingEngine
 {
 	DirectionalLight::DirectionalLight()
 	{
-		const glm::mat4 ortho = glm::ortho(-35.0f, 35.0f, -35.0f, 35.0f, 0.01f, 100.0f);
+		const glm::mat4 ortho = glm::ortho(-30.0f, 30.0f, -30.0f, 30.0f, 0.01f, 100.0f);
 		const glm::mat4 view = glm::lookAt(m_Parameters.Position, glm::vec3(0), glm::vec3(0, 1, 0));
 
 
